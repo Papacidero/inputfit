92e86d4aca8d12ea6940d3c2d28246cc0fd83d3e
Carlos Eduardo Papacidero <carlos.papacidero@venturus.org.br>
2015-07-08 09:32:54 -0300

    Some Plugin Info

--- js/inputfit.js
+++ js/inputfit.js
@@ -1,3 +1,9 @@
+/*! Input Fit Plugin - v0.0.1 - 2015-07-08
+ * https://github.com/Papacidero/inputfit
+ *
+ * Copyright (c) 2015 Carlos Eduardo Papacidero <papacidero@gmail.com>;
+ * Licensed under the MIT license */
+
 (function ($) {
 
     $.fn.inputFit = function (options) {
