d0e85ce8fe1c6f0fdda9a04a6f0e6c48b8fd9adf
Carlos Eduardo Papacidero <carlos.papacidero@venturus.org.br>
2015-07-08 09:25:14 -0300

    Start

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# inputfit
-Input Fit Plugin
+# Input Fit Plugin
+
+Makes the typed text fit into the input, resizing it up and down according the text size.
+
+## Use
+
+Needs Jquery library.
+
+```javascript
+#('input').inputfit();
+```
---
+++ js/inputfit.js
@@ -0,0 +1,84 @@
+(function ($) {
+
+    $.fn.inputFit = function (options) {
+
+        var defaults = {
+            'maxFontSize': 40,
+            'minFontSize': 5
+        };
+
+        var options = $.extend({}, defaults, options);
+
+
+        var originalFontSize = $(this).css('font-size');
+        var originalFontFamily = $(this).css('font-family');
+        var originalFontAlignment = $(this).css('text-align');
+        var originalFontWeight = $(this).css('font-weight');
+        var originalTextTransform = $(this).css('text-transform');
+        var maxWidth = $(this).width() - 5;
+        var maxHeight = $(this).height();
+
+        $(this).addClass('inputFit');
+        //$(this).after('<div id="temp" style="width:' + maxWidth + 'px;height:' + maxHeight + 'px;border: 1px solid red; float: left;"><span class="inputFit" style="display:block; border: 1px solid red; float: left;"></span></div>');
+		
+		$(this).after('<span class="inputFit" style="display:none; border: 1px solid red; float: left;"></span>');
+
+        $('span.inputFit').css({
+            'font-size': originalFontSize,
+            'font-family': originalFontFamily,
+            'text-align': originalFontAlignment,
+            'font-weight': originalFontWeight,
+            'text-transform': originalTextTransform
+        });
+
+
+        var actualWidth = '';
+        var actualHeight = '';
+        var actualFontSize = '';
+
+        function actualSizes() {
+            actualWidth = $('span.inputFit').width() + 5;
+            actualHeight = $('span.inputFit').height();
+            actualFontSize = parseInt($('span.inputFit').css('font-size'));
+        }
+
+        function resize() {
+
+            fontSize = actualFontSize * maxWidth / actualWidth * 0.9;
+            if (maxWidth < actualWidth) {
+                $('.inputFit').css('font-size', fontSize);
+            } else if (maxWidth > actualWidth) {
+                fontSize = actualFontSize * maxWidth / actualWidth * 0.9;
+                if (fontSize <= parseInt(originalFontSize)) {
+                    $('.inputFit').css('font-size', fontSize);
+                }
+            }
+        }
+
+        $(this).keydown(function (event) {
+
+            //Prevent SpaceBar
+            if (event.keyCode == 32) {
+                event.preventDefault();
+            }
+
+            //Using toUpperCase
+            $('span.inputFit').text($(this).val().toUpperCase());
+            actualSizes();
+            resize();
+        });
+
+        $(this).keyup(function () {
+            //Using toUpperCase
+            $('span.inputFit').text($(this).val().toUpperCase());
+            actualSizes();
+
+		if($(this).val() == ''){
+			$(this).css('font-size',originalFontSize);
+			}
+
+        });
+
+    };
+
+})(jQuery);
